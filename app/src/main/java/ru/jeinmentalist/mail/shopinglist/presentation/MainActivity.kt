13300553304436f042795ec356df01cb25093c73
Стас <cjb2018@yandex.ru>
2021-10-06 23:48:48 +0700

    creating basic packages

--- app/src/main/java/ru/jeinmentalist/mail/shopinglist/presentation/MainActivity.kt
+++ app/src/main/java/ru/jeinmentalist/mail/shopinglist/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package ru.jeinmentalist.mail.shopinglist
+package ru.jeinmentalist.mail.shopinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.jeinmentalist.mail.shopinglist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
